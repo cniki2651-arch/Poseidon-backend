f1c7dfc45e240742999a0dc6b80ae1db1148457d
Arlet Astorga <rosa.astorga@unmsm.edu.pe>
2026-06-23 14:42:40 -0500

    feat/security: limitar tamano de json entrante a 1mb para proteger el servidor

--- src/server.js
+++ src/server.js
@@ -9,7 +9,7 @@ require('./config/db');
 
 // Middlewares globales
 app.use(cors());
-app.use(express.json()); 
+app.use(express.json({ limit: '1mb' }));
 
 // ========================
 // RUTAS DE LA API
