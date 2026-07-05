115092e247ad92dd7f5e98eb42b65bbc7dd69c19
Khalep <khalepvelarde123@gmail.com>
2026-07-04 23:23:49 -0500

    Se añadio la ruta GET para listar las solicitudes pendientes con cálculo de deuda en el archivo retirosRoutes.js. Esta ruta es accesible solo para el rol de Jefe (1).

--- src/routes/retirosRoutes.js
+++ src/routes/retirosRoutes.js
@@ -2,9 +2,14 @@ const express = require('express');
 const router = express.Router();
 const { registrarSolicitudRetiro } = require('../controllers/retirosController');
 const { verificarToken, autorizarRoles } = require('../middlewares/authMiddleware');
+const { obtenerSolicitudesPendientes } = require('../controllers/retirosController');
 
 // RUTA POST: Crear solicitud de retiro
 // Acceso: Jefe (1) y Secretaría de Atención al Cliente (2)
 router.post('/solicitar', verificarToken, autorizarRoles(1, 2), registrarSolicitudRetiro);
+
+// RUTA GET: Listar solicitudes pendientes con cálculo de deuda
+// Acceso: Jefe (1)
+router.get('/pendientes', verificarToken, autorizarRoles(1), obtenerSolicitudesPendientes);
 
 module.exports = router;
